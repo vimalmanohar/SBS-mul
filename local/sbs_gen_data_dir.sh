2309f504b357b42ea6c975e96cbfb5b450b547e5
Vimal Manohar <vimal.manohar91@gmail.com>
2015-08-05 14:54:23 -0400

    vimal: Added unlabelled data preparation script

---
+++ local/sbs_gen_data_dir.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "sbs_gen_data_dir.sh <DATA> <data-dir>"
+  exit 1
+fi
+
+DATA=$1
+dir=$2
+
+mkdir -p data/local/$dir
+mkdir -p $dir
+
+find $DATA -name "*.wav" > data/local/$dir/wav.scp
+for x in `cat data/local/$dir/wav.scp`; do
+  y=`basename $x`
+  z=${y%*.wav}
+  echo $z $y
+done > $dir/wav.scp
+
+awk '{print $1" "$1}' $dir/wav.scp > $dir/utt2spk
+awk '{print $1" "$1}' $dir/wav.scp > $dir/spk2utt
+
+utils/fix_data_dir.sh $dir
