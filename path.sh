e305eac1bd6a549433f2b58fbc24a218f7d106f0
Vimal Manohar <vimal.manohar91@gmail.com>
2015-08-10 01:36:26 -0400

    vimal: Made run_dnn_semisup.sh to exit on error

--- path.sh
+++ path.sh
@@ -2,4 +2,4 @@ export KALDI_ROOT=../..
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/sph2pipe_v2.5/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/irstlm/bin/:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/kwsbin:$KALDI_ROOT/src/online2bin:$KALDI_ROOT/src/ivectorbin:$PWD:$PATH
 export LC_ALL=C
 export IRSTLM=$KALDI_ROOT/tools/irstlm
-export LD_LIBRARY_PATH=$KALDI_ROOT/tools/openfst/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/tools/openfst/lib
